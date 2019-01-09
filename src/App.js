3eae228d643b5788674ec7cad5e534ac2d2bf998
Pablo Terradillos <tehsis@gmail.com>
2019-01-09 18:19:45 -0300

    example 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newTodo: '',
+      todos: []
+    }
+    this.todoReady = this.todoReady.bind(this);
+    this.handleNewTodo = this.handleNewTodo.bind(this);
+    this.onTodoDone = this.onTodoDone.bind(this);
+  }
+
+  todoReady(ev) {
+    ev.preventDefault();
+    this.setState((prev) => {
+      prev.todos.push(prev.newTodo);
+      prev.newTodo = '';
+      return prev;
+    })
+  }
+
+  handleNewTodo(ev) {
+    const {value} = ev.currentTarget;
+    this.setState((prev) => {
+      prev.newTodo = value;
+      return prev;
+    })
+  }
+  
+  onTodoDone(id) {
+    return (ev) => {
+      this.setState((prev) => {
+        prev.todos.splice(id, 1);
+        return prev;
+      })
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>My Todo List</h1>
+        <form onSubmit={this.todoReady}>
+        <input type="text" onInput={this.handleNewTodo} value={this.state.newTodo} />
+        </form>
+
+        <ul>
+          {this.state.todos.map((todo, index) => <li>{todo}<button onClick={this.onTodoDone(index)}>Done</button></li> )}
+        </ul>
       </div>
     );
   }
